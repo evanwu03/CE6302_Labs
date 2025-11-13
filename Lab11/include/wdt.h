a413ae40ca95851ae8390c149c1fcb7162cbdcff
Evan Wu <ewu43951@gmail.com>
2025-11-13 15:12:57 -0600

    Added comments

--- Lab11/include/wdt.h
+++ Lab11/include/wdt.h
@@ -5,20 +5,21 @@
 #include <stdint.h>
 
 
+/// @brief Watchdog Timer instance structure
 struct wdt{
     uintptr_t base;
     uint16_t ctl
 ;};
 
 
-
+/// @brief Configuration structure for the Watchdog Timer
+/// Contains settings for mode, interval, clock source, and counter clear
 struct wdt_config_t {
     uint8_t mode_select;
     uint8_t interval_select;
     uint8_t clock_source;
     uint8_t counter_clear;
 };
-
 
 /// @brief Initializes the Watchdog Timer
 void WDT_init(struct wdt* wdt_instance, unsigned long base, struct wdt_config_t* config);
